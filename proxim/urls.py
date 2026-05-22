39b345f8bedc1ed5e3d350e1d8a67eb91c7266c5
amirmoussa01 <moussaamir979@gmail.com>
2026-05-22 21:27:59 +0100

    Integration des pages legales

--- proxim/urls.py
+++ proxim/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from admin_dashboard import views as adv
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,6 +20,17 @@ urlpatterns = [
     path('admin-dashboard/login/', adv.login_view, name='admin_login'),
     path('admin-dashboard/logout/', adv.logout_view, name='admin_logout'),
     path('admin-dashboard/', include('admin_dashboard.urls')),
+
+    path(
+        'legal/conditions-utilisation/',
+        TemplateView.as_view(template_name='legal/conditions_utilisation.html'),
+        name='conditions_utilisation',
+    ),
+    path(
+        'legal/politique-confidentialite/',
+        TemplateView.as_view(template_name='legal/politique_confidentialite.html'),
+        name='politique_confidentialite',
+    ),
 
 ]
 
